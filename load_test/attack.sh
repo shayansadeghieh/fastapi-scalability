edebf66ddfeedbb1dee4926a273a81419b7e09f9
Shayan Sadeghieh <shayan.sadeghieh@gmail.com>
2024-06-22 11:26:27 -0400

    latency linearly increases

--- load_test/attack.sh
+++ load_test/attack.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-# duration = 1 # number of seconds to make that request for 
-# rate = 1 # request per second
+# Variables
+duration=5 # number of seconds to make that request for 
+rate=3 # request per second
+timeout=60 #timeout for a request in seconds
 
-vegeta attack -duration=5s -rate=1 -targets=targets.list -output=results.bin
+vegeta attack -duration=${duration}s -timeout=${timeout}s -rate=${rate} -targets=targets.list -output=results.bin
 cat results.bin | vegeta plot > plot.html
 open -a "Google Chrome" plot.html
 
--- main.py
+++ main.py
@@ -14,25 +14,27 @@ class OrderRequest(BaseModel):
 async def hungry(order: OrderRequest):
     item = order.item.lower()
     if item == "burgers":
-        task = asyncio.create_task(make_burgers())
+        fries_task = asyncio.create_task(make_fries())
+        await fries_task          
+        burgers_task = await make_burgers()                  
     elif item == "fries":
         task = asyncio.create_task(make_fries())
     else:
         raise HTTPException(status_code=400, detail="Invalid item requested. Please specify 'burgers' or 'fries'.")
     
-    await task
+    # await task
     return f"{order.item.capitalize()} are ready!"
 
 async def make_burgers():
     # simulating i/o bound stuff, then you want to use asyncio.sleep(time)
-    print("Heating up stove top")
-    await asyncio.sleep(2)
+    print("Heating up stove top")    
+    time.sleep(2)
     print("Throw burgs on stove")
     await asyncio.sleep(1)
     print("Burgers are ready!")
 
 async def make_fries():
-    print("Heating up oil")
+    print("Heating up fry oil")
     await asyncio.sleep(1)
     print("Put fries into oil")
     await asyncio.sleep(1)
